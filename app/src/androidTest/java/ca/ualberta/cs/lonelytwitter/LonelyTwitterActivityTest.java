087313b0a99f147d15fcba567054a48104f797be
Fangting Chen <fc1@ualberta.ca>
2018-02-27 18:00:19 -0700

    lab7

--- app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivityTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivityTest.java
@@ -2,10 +2,16 @@ package ca.ualberta.cs.lonelytwitter;
 
 import android.app.Activity;
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.EditText;
+
+import com.robotium.solo.Solo;
 
 import junit.framework.TestCase;
 
 public class LonelyTwitterActivityTest extends ActivityInstrumentationTestCase2 {
+
+    private Solo solo;
+
 
     public LonelyTwitterActivityTest() {
         super(ca.ualberta.cs.lonelytwitter.LonelyTwitterActivity.class);
@@ -15,4 +21,28 @@ public class LonelyTwitterActivityTest extends ActivityInstrumentationTestCase2 
         Activity activity = getActivity();
 
     }
+
+    public void setUp() throws Exception{
+        solo = new Solo(getInstrumentation(), getActivity());
+
+    }
+
+    public void testAddingTweetToTheList(){
+        solo.assertCurrentActivity("Wrong activity",LonelyTwitterActivity.class);
+        solo.enterText((EditText) solo.getView(R.id.body), "Test Tweet!");
+        solo.clickOnButton("Save");
+
+        solo.enterText((EditText) solo.getView(R.id.body), "");
+
+        assertTrue(solo.searchText("Test Tweet!"));
+
+        solo.clickOnButton("Clear");
+        assertFalse(solo.searchText("Test Tweet!"));
+    }
+
+    @Override
+    public void tearDown() throws Exception{
+        solo.finishOpenedActivities();
+    }
+
 }
---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/EditTweetActivity.java
@@ -0,0 +1,14 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class EditTweetActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_tweet);
+    }
+
+}
